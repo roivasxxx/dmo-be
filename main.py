dc9151b5a7e29111233a08d2d9e88a7685e923ed
roivasxxx <tiragoshi@gmail.com>
2022-12-26 15:56:46 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask,jsonify,request
+
+app = Flask("DMO_BE")
+
+
+GRAPH_ROUTES=[
+    "ARTICULATIONS",
+    "HUNGARIAN"
+]
+
+def graph(type):
+    match type: 
+        case "ARTICULATIONS":return {"result":"arts"}
+        case "HUNGARIAN":return {"result":"hung"}
+
+@app.route("/graph",methods=["POST"])
+def root():
+    content_type=request.content_type
+    if content_type.lower() is not"application/json":
+        return "Invalid request Content-Type Header.",500
+    body=request.get_json()
+    if "graph_id" not in body:
+        return "No graph_id supplied in request body.",500
+    if "graph_id" in body and body["graph_id"] not in GRAPH_ROUTES:
+        return f"Invalid graph_id: {body['graph_id']} supplied to request body.",400
+    print(body["graph_id"],content_type)
+    return jsonify(graph(body["graph_id"])),200
+
